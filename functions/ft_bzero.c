684a4b505a60a1248c9434b35f5ab2745ec2fb85
Prag42 <pragathez42@gmail.com>
2021-05-26 19:06:54 +0800

    Reworked memset, Added ft_bzero

--- functions/ft_bzero.c
+++ functions/ft_bzero.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/26 15:35:22 by prag              #+#    #+#             */
-/*   Updated: 2021/05/26 19:05:54 by pmoorthy         ###   ########.fr       */
+/*   Created: 2021/05/26 18:55:07 by pmoorthy          #+#    #+#             */
+/*   Updated: 2021/05/26 19:06:12 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t len)
+void	ft_bzero(void	*s,	size_t n)
 {
-	size_t	k;
-	char	*p;
-
-	k = 0;
-	p = (char *)str;
-	while (k < len)
-	{
-		p[k] = c;
-		k++;
-	}
-	return (str);
+	ft_memset(s, 0, n);
 }
