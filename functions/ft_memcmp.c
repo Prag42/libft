58ed960d9d10d854421ad4a3d958ae9a78c1f228
Prag42 <pragathez42@gmail.com>
2021-05-26 21:37:44 +0800

    Improved readability on memcmp

--- functions/ft_memcmp.c
+++ functions/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 20:31:28 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/05/26 20:51:01 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/05/26 21:33:26 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
 	size_t	i;
+	char	*cast1;
+	char	*cast2;
 
 	i = 0;
+	cast1 = (char *)s1;
+	cast2 = (char *)s2;
 	while (i < n)
 	{
-		if (((unsigned char *)str1)[i] - ((unsigned char *)str2)[i] != 0)
-			return ((int)((unsigned char *)str1)[i] -
-			 ((unsigned char *)str2)[i]);
+		if (cast1[i] != cast2[i])
+			return ((unsigned char)str1[i] - ((unsigned char)str2)[i]);
 		i++;
 	}
 	return (0);
