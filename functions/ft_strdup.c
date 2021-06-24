31d5c346b45c47de1308176cec051b1e345632cf
Prag42 <pragathez42@gmail.com>
2021-06-24 19:25:50 +0800

    Added malloc functions of Part 1

---
+++ functions/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/24 19:13:27 by pmoorthy          #+#    #+#             */
+/*   Updated: 2021/06/24 19:25:24 by pmoorthy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*dup;
+	int		i;
+	int		msize;
+
+	msize = 0;
+	i = 0;
+	while (str[msize])
+		size++;
+	dup = malloc(sizeof(char) * (size + 1));
+	if (dup == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
