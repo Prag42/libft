1f7977d71436359f2d7a2cc7205e83c167c924ab
Prag42 <pragathez42@gmail.com>
2021-07-04 01:27:22 +0800

    Corrected strnstr and strrchr

--- functions/ft_strnstr.c
+++ functions/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 18:25:51 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/07/03 18:46:31 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/07/04 01:27:01 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ char	*ft_strnstr(const char *str1, const char *str2, size_t n)
 	size_t	i;
 	size_t	j;
 
+	if (!*str2)
+		return ((char *)str1);
 	i = 0;
-	while (i < n && str1[i] != '\0')
+	while (i < n && str1[i])
 	{
 		j = 0;
-		while (i + j < n && str1[i + j] == str2[j] && str2[j] != '\0')
+		while (i + j < n && str1[i + j] == str2[j])
+		{
+			if (str2[j + 1] == '\0')
+				return ((char *)str1 + i);
 			j++;
-		if (str2[j] == '\0')
-			return ((char *)str1 + i);
+		}
 		i++;
 	}
 	return (NULL);
--- functions/ft_strrchr.c
+++ functions/ft_strrchr.c
@@ -6,23 +6,23 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 17:55:50 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/07/03 18:43:20 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/07/04 01:13:01 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strrchr(const char *str, int c)
-{
-	int		i;
+	{
+	int	i;
 
-	i = ft_strlen(str);
-	if (c == 0)
-		return ((char *)str + i);
+	i = 0;
+	while (str[i])
+		i++;
 	while (i >= 0)
 	{
-		if (str[i] == c)
-			return ((char *)str + i);
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
 		i--;
 	}
 	return (NULL);
