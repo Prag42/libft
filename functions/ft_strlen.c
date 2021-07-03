2a8a5ed431fcebe6e05b1b2436e6e7e562e266d2
Prag42 <pragathez42@gmail.com>
2021-07-03 17:52:13 +0800

    Fixed ft_strlen

--- functions/ft_strlen.c
+++ functions/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 14:09:19 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/07/03 16:37:31 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/07/03 17:51:06 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_strlen(const char *str)
 {
-	int	len;
+	size_t	len;
 
 	len = 0;
 	while (str[len])
 	{
 		len++;
 	}
-	return (str[len]);
+	return (len);
 }
--- functions/ft_strrchr.c
+++ functions/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 17:55:50 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/07/03 16:21:07 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/07/03 17:45:43 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*ft_strrchr(const char *str, int c)
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)src)[i] == (unsigned char)c)
-			return (src + i);
+		if (((unsigned char *)str)[i] == (unsigned char)c)
+			return (str + i);
 		i++;
 	}
 	return (0);
