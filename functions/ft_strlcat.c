9d5d2ef1f954e73bc2ed7792d7d35490db4c61e6
Prag42 <pragathez42@gmail.com>
2021-07-03 19:02:55 +0800

    Strlcat done

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pmoorthy <pmoorthy@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 15:59:05 by pmoorthy          #+#    #+#             */
-/*   Updated: 2021/07/03 17:51:59 by pmoorthy         ###   ########.fr       */
+/*   Updated: 2021/07/03 19:02:36 by pmoorthy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	dstlen;
-	size_t	srclen;
+	size_t	j;
+	size_t	k;
 
-	i = 0;
-	dstlen = ft_strlen(dst);
-	srclen = ft_strlen(src);
-	if (size - 1 <= dstlen)
-		return (srclen + size);
-	while (dstlen + i < size - 1)
+	j = 0;
+	k = 0;
+	while (dst[j] && j < size)
+		j++;
+	while ((src[k]) && ((j + k + 1) < size))
 	{
-		dst[dstlen + i] = src[i];
-		i++;
+		dst[j + k] = src[k];
+		k++;
 	}
-	dst[dstlen + i] = '\0';
-	return (dstlen + srclen);
+	if (j != size)
+		dst[j + k] = '\0';
+	return (j + ft_strlen(src));
 }
